896b9df9ff595d56b6576c87818cd6f857716b65
gva2024cl <155015386+gva2024cl@users.noreply.github.com>
2024-02-20 10:48:52 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+    .Bloque1 {background-color: gold}
+    .Bloque2 {background-color: silver;
+              margin-bottonm: 5px}
+    .Bloque1 {border: 2px solid}
+    .Bloque2 {border: 2px solid}
+    .Bloque1 {margin-top: 5px;
+              margin-bottom: 5px}
